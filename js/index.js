144d28970c495d54b7f5c818d261100a7a0bb75f
fuyingdi <fuyingdi@126.com>
2019-12-24 15:19:45 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -161,6 +161,7 @@ new Vue({
         .then(function(response){
           if(response.data.new==true)
           {
+            console.log("yesyesyes");
             that.getMessage();
           }
           else{
@@ -173,22 +174,29 @@ new Vue({
         })
     },
     getMessage(){
+      var that = this;
       axios
       .get(this.domain+'/api/messages/'+this.userid)
       .then(function(response){
-        this.messageList = this.messageList.concat(response.data.reverse());
+        // this.messageList = this.messageList.concat(response.data.messages);
         //if(messageList.length>10)messageList.shift();
-        // messageList.push(
-        //   {
-        //     type:"text",
-        //     content:response.data,
-        //     self:false,
-        //   }
+        for(var i=0;i<response.data.messages.length;i++)
+        {
+          var a = JSON.parse(response.data.messages[i]);
+          console.log(a);
+          that.messageList.push(
+            {
+              type:"text",
+              content:a.content,
+              self:false,
+            })
+        }
         console.log(response.data);
       })
       .catch()
     },
     sendMessage(){
+      var that = this;
       let data = {
         to:this.currentchat.username,
         type:"text",
