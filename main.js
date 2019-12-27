a6afcf0c7567483f8733d8fe0590f23181f7e4ba
fuyingdi <fuyingdi@126.com>
2019-12-27 13:59:35 +0800

    123

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const { execFile, execFileSync } = require('child_process');
 const { app, BrowserWindow } = require('electron')
 const electron = require('electron');
 const dgram = require('dgram')
+const fs = require('fs')
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
 let win
